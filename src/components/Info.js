eb18efec7c0404420c9caf7a1e7620c6fb92672d
ceejayv328 <ceejayv328@gmail.com>
2022-06-23 14:31:23 +0800

    Initial commit

---
+++ src/components/Info.js
@@ -0,0 +1,12 @@
+import react from "react";
+
+export default function Info() {
+    return (
+        <main>
+            <image className="info--profilePicture"/>
+            <label className="info--name">CeeJay</label>
+            <label className="info--profession">Software Developer</label>
+            <label className="info--website">google.com</label>
+        </main>
+    )
+}
